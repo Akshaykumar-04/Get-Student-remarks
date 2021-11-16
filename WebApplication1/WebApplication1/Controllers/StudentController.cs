f278f14c70e47c1497ff1ec7fda3b797cccc9a17
AkshayKumar-04 <[akshaya457@gmail.com]>
2021-11-16 18:29:34 +0530

    first commit

---
+++ WebApplication1/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentDB st = new StudentDB();
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult All()
+        {
+            List<StudentResult> str = st.StudentResults.ToList();
+            return PartialView("_Student",str);
+        }
+        public PartialViewResult Pass()
+        {
+            List<StudentResult> str = st.StudentResults.Where(s => s.remarks.Equals("P")).ToList();
+            return PartialView("_Student", str);
+        }
+        public PartialViewResult fail()
+        {
+            List<StudentResult> str = st.StudentResults.Where(s => s.remarks.Equals("F")).ToList();
+            return PartialView("_Student", str);
+        }
+    }
+}
